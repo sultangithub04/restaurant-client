efb82b8d37212737fc466599c3da3208be0ea964
sultangithub04 <sult_04@yahoo.com>
2024-12-22 22:28:17 +0600

    project setup update

--- src/main.jsx
+++ src/main.jsx
@@ -1,27 +1,21 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-<<<<<<< HEAD
-
-import {
-
-  RouterProvider,
-} from "react-router-dom";
-import router from './routes/Routes.jsx';
-import AuthProvider from './providers/AuthProvider.jsx';
-
-
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <AuthProvider>
-      <RouterProvider router={router} />
-    </AuthProvider>
-=======
 import App from './App.jsx'
 
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <div>Hello world!</div>,
+  },
+]);
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
->>>>>>> a1cfd9d87526f6ea3f3689ede68e05d4f9d7b6c1
+    <RouterProvider router={router} />
   </StrictMode>,
 )
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,21 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-<<<<<<< HEAD
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
-
   theme: {
     extend: {},
   },
   plugins: [require('daisyui'),],
-=======
-  content: [],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
->>>>>>> a1cfd9d87526f6ea3f3689ede68e05d4f9d7b6c1
 }
 
